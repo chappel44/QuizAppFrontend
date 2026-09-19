0acba1e994f53cb735c3d65a84f03a9f3f135ad1
Chris H <chrishappel10@gmail.com>
2026-09-19 18:44:06 -0400

    Created a sign in/sign up page

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,174 @@
-import Image from "next/image";
+"use client"
+
+import { AtSign, Eye, EyeOff, Lock, LockIcon, LockKeyhole, Mail, Plane, PlaneIcon } from "lucide-react";
+import React, { useState } from "react";
+import { handleSignin, handleSignup } from "./actions";
+
+export interface LoginType {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
 
 export default function Home() {
+  const [formData, setFormData] = useState<LoginType>({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: ""
+  })
+  const [loginOpen, setLoginOpen] = useState(true);
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const id = String(e.target.id)
+    setFormData(formData => ({...formData, [id]: e.target.value}))
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    
+    if(loginOpen) {
+      const res = await handleSignin(formData)
+
+      if (res.success && res.token) {
+        localStorage.setItem("token", res.token)
+        alert("You have signed in successfully")
+      }
+      else {
+        alert("Invalid credentials")
+      }
+    }
+    else {
+      const res = await handleSignup(formData)
+
+      if (res.success) {
+        alert("You have signed up successfully")
+        setLoginOpen(true)
+      }
+      else {
+        alert("Error signing up")
+      }
+    }
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="w-screen h-screen flex justify-center ">
+      <div className="flex flex-col justify-center px-4 w-lg">
+        <div className="flex flex-col bg-purple-50 rounded-xl shadow-lg px-8 py-4 items-center">
+          <PlaneIcon className="w-15 h-15 bg-purple-300 p-1 rounded-full text-purple-600 border border-purple-400 mb-2"/>
+          <h1 className="text-black/90 font-semibold text-2xl mb-1">Flight School {loginOpen ? "Login" : "Sign Up"}</h1>
+          <p className="flex flex-col text-black/60 mb-2">Sign in to take assessments</p>
+          <form
+            onSubmit={(e) => handleSubmit(e)}
+            className="space-y-2 w-full"
           >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <div className="flex flex-col text-gray-700">
+              <label 
+                htmlFor="email"
+                className="text-sm text-gray-700/75"
+              >
+                Email
+              </label>
+              <div className="bg-purple-100 border border-purple-300 rounded px-2 py-1 flex gap-2 focus-within:outline focus-within:outline-2 focus-within:outline-purple-500">
+                <AtSign className="text-purple-400" />
+                <input
+                  onChange={handleInputChange}
+                  value={formData.email}
+                  id="email"
+                  required
+                  className="focus:outline-none w-full"
+                />
+              </div>
+            </div>
+            <div className={`flex flex-col text-gray-700 ${loginOpen ? "mb-4" : ""}`}>
+              <label
+                htmlFor="password"
+                className="text-sm text-gray-700/75"
+              >
+                Password
+              </label>
+              <div className="bg-purple-100 border-purple-300 border rounded px-2 py-1 flex gap-2 focus-within:outline focus-within:outline-2 focus-within:outline-purple-500">
+                { showPassword ?
+                <EyeOff onClick = {() => setShowPassword(false)} className="text-purple-400"/>
+                :
+                <Eye onClick = {() => setShowPassword(true)} className="text-purple-400"/>
+                }
+                <input 
+                  onChange = {(e) => handleInputChange(e)}
+                  value={formData.password}
+                  required
+                  id = "password" 
+                  type = {showPassword ? "" : "password"}
+                  className="focus:outline-none w-full" 
+                />
+              </div>
+            </div>
+            {
+              !loginOpen && (
+              <>
+                <div className="flex flex-col text-gray-700">
+                  <label 
+                    htmlFor="firstName"
+                    className="text-sm text-gray-700/75"
+                  >
+                    First Name
+                  </label>
+                  <div className="bg-purple-100 border-purple-300 border rounded px-2 py-1 focus-within:outline focus-within:outline-2 focus-within:outline-purple-500">
+                    <input 
+                      onChange = {(e) => handleInputChange(e)}
+                      value={formData.firstName}
+                      required
+                      id = "firstName" 
+                      className="focus:outline-none w-full" 
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col text-gray-700">
+                  <label 
+                    htmlFor="lastName"
+                    className="text-sm text-gray-700/75"
+                  >
+                    Last Name
+                  </label>
+                  <div className="bg-purple-100 border-purple-300 border rounded px-2 py-1 focus-within:outline focus-within:outline-2 focus-within:outline-purple-500">
+                    <input 
+                      onChange = {(e) => handleInputChange(e)}
+                      value={formData.lastName}
+                      required
+                      id = "lastName" 
+                      className=" focus:outline-none w-full" 
+                    />
+                  </div>
+                </div>
+              </>
+              )
+            }
+            <div className="flex flex-col justify-center mb-4">
+              <button 
+                type="submit"
+                className="bg-purple-500 text-white px-2 py-1 rounded cursor-pointer h-10 hover:bg-purple-600"
+              >
+                {loginOpen ? "Login" : "Sign Up"}
+              </button>
+            </div>
+            {
+            loginOpen ?
+            <p className="text-gray-800 flex gap-1 justify-center">
+              Dont have an account? 
+              <button type = "button" onClick = {() => setLoginOpen(false)} className="text-purple-500 cursor-pointer">
+                Sign up here
+              </button>
+            </p>
+            :
+              <button type = "button" onClick = {() => setLoginOpen(true)} className="text-purple-500 cursor-pointer flex justify-center w-full">
+                Back to sign in
+              </button>
+            }
+          </form>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
